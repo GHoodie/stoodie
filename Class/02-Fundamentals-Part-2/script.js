36ad17dc4ed158258c963394a7e48b9a14a88d3f
GHoodie <chlwltn3246@knou.ac.kr>
2024-03-13 10:44:45 +0900

    Test : Study of Arrow functions - 35

--- Class/02-Fundamentals-Part-2/script.js
+++ Class/02-Fundamentals-Part-2/script.js
@@ -33,7 +33,7 @@ console.log(appleJuice);
 const appleOrageJuice = fruitProcessor(2, 4);
 console.log(appleOrageJuice);
 
-*/
+
 
 // Function declaration
 function calcAge1(birthYear) {
@@ -50,3 +50,20 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+*/
+
+// Arrow Function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1998, 'Bob'));
