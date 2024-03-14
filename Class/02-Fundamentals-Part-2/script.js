bdec665f8d880779e23d892ce11be549aeb826a3
GHoodie <chlwltn3246@knou.ac.kr>
2024-03-14 14:11:13 +0900

    Test : Reviewing Functions - 37

--- Class/02-Fundamentals-Part-2/script.js
+++ Class/02-Fundamentals-Part-2/script.js
@@ -68,7 +68,7 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1998, 'Bob'));
 
-*/
+
 
 function cutFruitPieces(fruit) {
     return fruit * 4;
@@ -83,3 +83,26 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
+
+
+*/
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already reitred`);
+        return -1;
+    }
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
